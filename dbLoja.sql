6884aebb3977d6e9ee34bb34c754aecebe6a6cda
JOAO OTAVIO DUARTE DE SOUZA <joao.odsouza@senacsp.edu.br>
2022-11-01 16:39:40 -0300

    criando banco de dados loja

---
+++ dbLoja.sql
@@ -0,0 +1,52 @@
+ -- Apagar o banco de dados
+drop database dbLoja;
+
+ -- criando banco de dados
+ create database dbLoja;
+
+-- Acessar o banco de dados
+use dbLoja;
+
+-- Visualizar o banco de dados
+show database;
+
+-- Visualizar a tabela
+show tables;
+
+-- Criando tabela para o banco de dados
+create table tbFuncionario(
+codfunc int auto_increment,
+nome varchar (50),
+email varchar (14),
+cpf char(14),
+tel char(10),
+primary key(codfunc)
+);
+
+create table tbUsuario(
+codusu int auto_increment,
+nome varchar(15),
+senha varchar(14),
+primary key (codusu)
+);
+
+-- visualizar a tabelas bd
+show tables;
+
+--visualizando a estruturas das tabelas
+desc tbFuncionario;
+desc tbUsuario;
+
+-- incerindo valores na tabelas
+insert into tbFuncionario(nome,email,cpf,tel)
+values('João Otavio','jotavio250@gmail.com','28542968473','955287168');
+insert into tbFuncionario(nome,email,cpf,tel)
+values('Lucas Calvo','lucas@gmail.com','63846209348','993482588');
+insert into tbUsuario(nome,senha)
+values('João Otavio','cursos143');
+insert into tbUsuario(nome,senha)
+values('Lucas Calvo','cursos543');
+
+-- visualizando o registro da tabela 
+select * from tbFuncionario;
+select * from tbUsuario;
